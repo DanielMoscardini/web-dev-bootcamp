a7eaf99d20b6f7138b25421f13bf1e812ef8aa0e
Daniel Bolonha Moscardini <moscardinibdaniel@gmail.com>
2023-04-21 18:45:54 -0300

    Ending newsletter API

--- 17 - APIs/Newsletter-Signup/app.js
+++ 17 - APIs/Newsletter-Signup/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
 
 const app = express();
 
@@ -12,13 +13,49 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-  let name = req.body.inputName;
-  let lastName = req.body.inputLastName;
-  let email = req.body.inputEmail;
+  const name = req.body.inputName;
+  const lastName = req.body.inputLastName;
+  const email = req.body.inputEmail;
 
-  console.log(name);
-  console.log(lastName);
-  console.log(email);
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed',
+        merge_fields: {
+          FNAME: name,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = 'https://us17.api.mailchimp.com/3.0/lists/listId';
+
+  const options = {
+    method: 'POST',
+    auth: 'daniel1:APIKey'
+  };
+
+  const request = https.request(url, options, response => {
+    if (response.statusCode >= 200 && response.statusCode <= 299) {
+      res.sendFile(`${__dirname}/success.html`);
+    } else {
+      res.sendFile(`${__dirname}/failure.html`);
+    }
+
+    response.on('data', data => {
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
+});
+
+app.post('/failure', (req, res) => {
+  res.redirect('/');
 });
 
 app.listen(3000, () => {
