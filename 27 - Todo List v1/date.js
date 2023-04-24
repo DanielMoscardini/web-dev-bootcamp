12f089d1dae9c6fd99655b6dedbe5c83bf91f7bf
Daniel Bolonha Moscardini <moscardinibdaniel@gmail.com>
2023-04-24 11:56:08 -0300

    Ending To Do List with EJS

---
+++ 27 - Todo List v1/date.js
@@ -0,0 +1,12 @@
+const getDate = () => {
+  let today = new Date();
+  let options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+  };
+
+  return today.toLocaleDateString('en-US', options);
+};
+
+module.exports = getDate;
